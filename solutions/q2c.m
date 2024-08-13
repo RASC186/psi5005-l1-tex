d961bcff1aa4d18dbb489222fc338233640c641d
Ramon Andrade Sousa Carvalho <ramon.andrade@live.com>
2024-08-13 08:20:13 -0300

    Add question 2

---
+++ solutions/q2c.m
@@ -0,0 +1,23 @@
+clear;
+clc;
+
+hbar = 6.62607015E-34;
+me = 9.109E-31;
+m = 0.4*me;
+V0 = (224E-3)*(1.6022E-19);
+L = 10E-10;
+
+E = [0:0.01*V0:V0];
+
+eta = E/V0;
+
+alpha = sqrt(2*m*V0*(1-eta))/hbar;
+
+T = (1 + ((sinh(alpha.*L)).^2)./(4*eta.*(1-eta)) ).^-1;
+
+fig = figure(1)
+plot(E,T);
+title('T x E (m=0.4m_{e}, V_{0}=0.224[eV], L=1[nm])');
+xlabel('E [J]');
+ylabel('T');
+xlim([0 V0]);
