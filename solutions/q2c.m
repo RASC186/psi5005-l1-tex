47ff33ed75d80a299cbb7fa1fbd76f462cb15bdc
Ramon Andrade Sousa Carvalho <ramon.andrade@live.com>
2024-08-13 21:58:14 -0300

    Fix question 2 plots

--- solutions/q2c.m
+++ solutions/q2c.m
@@ -1,7 +1,7 @@
 clear;
 clc;
 
-hbar = 6.62607015E-34;
+hbar = 6.62607015E-34/(2*pi);
 me = 9.109E-31;
 m = 0.4*me;
 V0 = (224E-3)*(1.6022E-19);
@@ -20,4 +20,4 @@ plot(E,T);
 title('T x E (m=0.4m_{e}, V_{0}=0.224[eV], L=1[nm])');
 xlabel('E [J]');
 ylabel('T');
-xlim([0 V0]);
+saveas(fig, 'q2c.png', 'png');
--- solutions/q2d.m
+++ solutions/q2d.m
@@ -1,7 +1,7 @@
 clear;
 clc;
 
-hbar = 6.62607015E-34;
+hbar = 6.62607015E-34/(2*pi);
 me = 9.109E-31;
 m = 0.067*me;
 V0 = (224E-3)*(1.6022E-19);
@@ -9,11 +9,12 @@ V0 = (224E-3)*(1.6022E-19);
 eta = 3/4;
 alpha = sqrt(2*m*V0*(1-eta))/hbar;
 
-L = [0:1E-11:100E-9];
+L = [0:1E-11:10E-9];
 T = (1 + ((sinh(alpha.*L)).^2)./(4*eta.*(1-eta)) ).^-1;
 
-figure(1)
+fig = figure(1)
 plot(L,T);
 title('T x L (m=0.067m_{e}, E=(3/4)V_{0})');
 xlabel('L [m]');
 ylabel('T');
+saveas(fig, 'q2d.png', 'png');
